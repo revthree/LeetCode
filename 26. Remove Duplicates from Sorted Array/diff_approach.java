1bfc1fb95eb070d3c3c9c8d7c33b0b162a13931d
Rev <147642768+revthree@users.noreply.github.com>
2025-02-06 17:16:10 +0530

    Update diff_approach.java

--- 26. Remove Duplicates from Sorted Array/diff_approach.java
+++ 26. Remove Duplicates from Sorted Array/diff_approach.java
@@ -1,1 +1,12 @@
-
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int index=0;
+        for(int i=0; i<nums.length; i++){
+            if(index==0 ||  nums[index-1]!=nums[i]){
+              nums[index]=nums[i];
+              index++;
+            }
+        }
+        return index;
+    }
+}
