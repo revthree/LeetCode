7d330dd1cc20c2b71312d9b174fa895cb0fcd5a3
Rev <147642768+revthree@users.noreply.github.com>
2025-02-06 17:09:17 +0530

    Update submitted.java

--- 26. Remove Duplicates from Sorted Array/submitted.java
+++ 26. Remove Duplicates from Sorted Array/submitted.java
@@ -1,1 +1,15 @@
-
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int index=0;
+        for(int i=0; i<nums.length; i++){
+            if(i<nums.length-1 && nums[i]==nums[i+1]){
+                continue;
+            }else{
+                nums[index]=nums[i];
+                index++;
+            }
+            
+        }
+        return index;
+    }
+}
