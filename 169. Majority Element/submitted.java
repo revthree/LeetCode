85f8e3ffbff46a5b2293733006b95daeb60efc27
Rev <147642768+revthree@users.noreply.github.com>
2025-02-07 18:16:13 +0530

    Update submitted.java

--- 169. Majority Element/submitted.java
+++ 169. Majority Element/submitted.java
@@ -1,1 +1,21 @@
+// solved using Moor'es voting algorithm
+class Solution {
+    public int majorityElement(int[] nums) {
+        int candidate=0;   
+        int count=0; //points
+        for(int i=0; i<nums.length; i++){
 
+            if(count==0){
+                candidate=nums[i];
+            }
+            if(candidate==nums[i]){
+                count++;
+            }else{
+                count--;
+            }
+
+        }
+        return candidate;
+        
+    }
+}
